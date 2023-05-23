a51e90ce7454e09056a66555cad2cd7ef1b1970d
Yahia-EL-Alfy <90115669+Yahia-EL-Alfy@users.noreply.github.com>
2023-05-23 17:34:23 +0300

    class user added

---
+++ Al-Akeel-project/src/main/java/model/User.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.io.Serializable;
+
+
+
+
+public class User implements Serializable {
+	
+
+	private int id;
+	
+
+	private String name;
+	
+
+	private String role;
+	
+
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
